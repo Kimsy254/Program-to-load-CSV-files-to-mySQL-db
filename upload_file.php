07e9b0840927ee306dbfaaeffdd65a1d32d24587
kimsy254 <livingstonkimani@gmail.com>
2022-05-15 13:01:10 +0300

    upload functionalities

---
+++ upload_file.php
@@ -0,0 +1,22 @@
+<?php
+// db file
+require 'db.php';
+ 
+if (isset($_POST['submit']))
+{
+ 
+    // Allowed mime types
+    $fileMimes = array(
+        'text/x-comma-separated-values',
+        'text/comma-separated-values',
+        'application/octet-stream',
+        'application/vnd.ms-excel',
+        'application/x-csv',
+        'text/x-csv',
+        'text/csv',
+        'application/csv',
+        'application/excel',
+        'application/vnd.msexcel',
+        'text/plain'
+    );
+ 
