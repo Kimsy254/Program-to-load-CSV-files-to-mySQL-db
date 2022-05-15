91f8cc2a087b0ae0ffbbec129bc6f82842416250
kimsy254 <livingstonkimani@gmail.com>
2022-05-15 13:10:55 +0300

    connecting to mySQL db

--- db.php
+++ db.php
@@ -2,6 +2,9 @@
 // Host, username, password, database.
 $con = mysqli_connect("localhost","root","","club");
 if (mysqli_connect_errno())
-
+{
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    die();
+}
 
 ?>
